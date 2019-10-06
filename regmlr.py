64c81b374b65d08b0569361d8407fd659b8dd394
Yihong Gu <wmyw96@126.com>
2019-10-06 16:15:15 +0800

    add resample

--- regmlr.py
+++ regmlr.py
@@ -16,16 +16,11 @@ from scipy.stats import kde
 import seaborn as sns
 from tensorflow.python.training.moving_averages import assign_moving_average
 
-'''
-2019-10-04
-CUDA_VISIBLE_DEVICES=3 python mnistml.py --num_hidden 1000,1000,1 --weight_decay 0,0,0 --regw 0.01 --save_log_dir ../../data/approximate-nn/logs/mnist-l21-3l-n1 --lr 1e-4 --decay 1.0
-'''
-
 
 # Parse cmdline args
 parser = argparse.ArgumentParser(description='MNIST')
 
-parser.add_argument('--x_dim', default=28*28, type=int)
+parser.add_argument('--x_dim', default=1, type=int)
 parser.add_argument('--gpu', default=-1, type=int)
 parser.add_argument('--num_hidden', default='100,10000,1', type=str)
 parser.add_argument('--weight_decay', default='0.0,0.01,1.0', type=str)
@@ -38,7 +33,6 @@ parser.add_argument('--save_log_dir', default='logs/sin1d3-1000', type=str)
 parser.add_argument('--train', default='all', type=str)
 parser.add_argument('--save_weight_dir', type=str, default='../../data/approximate-nn/logs/sin1d3-joint')
 parser.add_argument('--regw', default=0.01, type=float)
-parser.add_argument('--nclass', default=10, type=int)
 
 args = parser.parse_args()
 
@@ -65,39 +59,47 @@ def feed_forward(x, num_hidden, decay, activation, is_training):
     layers = [tf.identity(x)]
     l2_loss = tf.constant(0.0)
     l2_lloss = []
+    l2_loss_rsp = tf.constant(0.0)
+    l2_lloss_rsp = []
     inp_dim = int(x.get_shape()[-1])
+    ip_weights = []
+    pre_pnorm = tf.ones([inp_dim, ])
     for _ in range(depth):
         print('layer {}, num hidden = {}, activation = {}, decay = {}'.format(_, num_hidden[_], activation[_], decay[_]))
         init = tf.random_normal_initializer(mean=0,stddev=1/np.sqrt(inp_dim))
-        #if _ + 1 == depth:
-        #    init = tf.zeros_initializer()
-        #cur_layer = tf.layers.dense(layers[-1], num_hidden[_], name='dense_' + str(_), 
-        #                                activation=activation[_], use_bias=True,
-        #                                kernel_initializer=init)
-        with tf.variable_scope('dense_' + str(_), reuse=False):
-            w = tf.get_variable(name='kernel', shape=[inp_dim, num_hidden[_]], initializer=init)
-            b = tf.get_variable(name='bias', shape=[1, num_hidden[_]], initializer=tf.zeros_initializer())
-            cur_layer = tf.matmul(layers[-1], w) + b
-            if activation[_] is not None:
-                print('use activation {}'.format(activation[_]))
-                cur_layer = tf.nn.tanh(cur_layer)
-        #cur_layer = cur_layer / inp_dim
-        #if activation[_] is not None:
-        #    cur_layer = activation[_](cur_layer)
-        with tf.variable_scope('dense_' + str(_), reuse=True):
-            w = tf.get_variable('kernel')           # [1, m]        
+
+        with tf.variable_scope('nn', reuse=False):
+            with tf.variable_scope('dense_' + str(_), reuse=False):
+                w = tf.get_variable(name='kernel', shape=[inp_dim, num_hidden[_]], initializer=init)
+                b = tf.get_variable(name='bias', shape=[1, num_hidden[_]], initializer=tf.zeros_initializer())
+                cur_layer = tf.matmul(layers[-1], w) + b
+                if activation[_] is not None:
+                    print('use activation {}'.format(activation[_]))
+                    cur_layer = tf.nn.tanh(cur_layer)
+   
         inp_dim = num_hidden[_]
         
         #reg = tf.reduce_mean(tf.reduce_sum(tf.square(w), 0))
         reg = tf.reduce_sum(tf.square(tf.reduce_mean(tf.abs(w), 1) * inp_dim)) / (inp_dim ** 2)
-        #if _ == 1:
-        #    reg = tf.reduce_sum(tf.abs(w)) * decay[_] / num_hidden[_]
-        #else:
-        #    reg = tf.reduce_sum(tf.square(w)) * decay[_] / num_hidden[_]
+        with tf.variable_scope('ip', reuse=False):
+            with tf.variable_scope('imp_weight_' + str(_), reuse=False):
+                p_weight = tf.get_variable(name='ipweight', shape=[num_hidden[_]], initializer=tf.zeros_initializer())
+                p_norm = tf.exp(p_weight)
+                p_norm = p_norm / tf.reduce_sum(p_norm)
+                p_norm = p_norm * num_hidden[_]
+        ip_weights.append(p_norm)
+
+        pre_pnorm_ = tf.expand_dims(pre_pnorm, 1)
+        p_norm_ = tf.expand_dims(p_norm, 0)
+        reg_resample = \
+            tf.reduce_sum(tf.square(tf.reduce_mean(tf.abs(w) * p_norm_, 1) * inp_dim) / pre_pnorm) / (inp_dim ** 2)
         l2_lloss.append(reg)
         l2_loss += reg
+
+        l2_lloss_rsp.append(reg_resample)
+        l2_loss_rsp += reg_resample
         layers.append(cur_layer)
-    return layers[-1], l2_loss, layers, l2_lloss
+    return layers[-1], l2_loss, layers, l2_lloss, l2_loss_rsp, l2_lloss_rsp, ip_weights
 
 
 def show_variables(domain, cl):
@@ -111,7 +113,6 @@ def tf_add_grad_noise(all_grads, temp, lr):
     for g, v in all_grads:
         if g is not None and len(g.get_shape()) == 2:
             g = g + tf.sqrt(lr) * temp * tf.random_normal(shape=v.get_shape(),
-            #g = g * int(v.get_shape()[0]) + tf.sqrt(lr) * temp * tf.random_normal(shape=v.get_shape(), 
                 mean=0, stddev=1.0/int(v.get_shape()[0]))
         noise_grads.append((g, v))
     return noise_grads
@@ -119,45 +120,41 @@ def tf_add_grad_noise(all_grads, temp, lr):
 
 def build_model(num_hidden, decay, activation):
     x = tf.placeholder(dtype=tf.float32, shape=[None, args.x_dim])
-    y = tf.placeholder(dtype=tf.int64, shape=[None])
-    onehot_y = tf.one_hot(y, args.nclass)
+    y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+    layer2_copy = tf.placeholder(dtype=tf.float32, shape=[None, num_hidden[1]])
     is_training = tf.placeholder(dtype=tf.bool, shape=[])
     lr_decay = tf.placeholder(dtype=tf.float32, shape=[])
 
     with tf.variable_scope('network'):
-        out, reg, layers, regs = feed_forward(x, num_hidden, decay, activation, is_training)
+        out, reg, layers, regs, reg_rsp, regs_rsp, ip_weights = \
+            feed_forward(x, num_hidden, decay, activation, is_training)
 
-    log_y = tf.nn.softmax_cross_entropy_with_logits(labels=onehot_y, logits=out, dim=1)
+    rmse_loss = tf.reduce_mean(tf.reduce_sum(tf.square(y - out), 1))
+    loss = rmse_loss + reg * args.regw
 
-    acc_loss = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), y), tf.float32))
-    entropy_loss = tf.reduce_mean(log_y)
-    loss = entropy_loss + reg * args.regw
+    rsp_loss = reg_rsp
 
-    all_weights = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='network')
+    all_weights = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='network/nn')
+    ip_weights = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='network/ip')
+
     show_variables('All Variables', all_weights)
-    last_layer_weights = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 
-        scope='network/dense_{}'.format(len(num_hidden) - 1))
-    show_variables('Last Layer Variables', last_layer_weights)
-    
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope='network')
-    for item in update_ops:
-        print('Update {}'.format(item))
+    show_variables('Importance Weight Variables', ip_weights)
 
     all_op = tf.train.AdamOptimizer(args.lr * lr_decay)
     all_grads = all_op.compute_gradients(loss=loss, var_list=all_weights)
-
-    noise_grads = tf_add_grad_noise(all_grads, 1e-3, args.lr * lr_decay)
+    noise_grads = tf_add_grad_noise(all_grads, 1e-1, args.lr * lr_decay)
     all_train_op = all_op.apply_gradients(grads_and_vars=noise_grads)
 
-    lst_op = tf.train.AdamOptimizer(args.lr * lr_decay)
-    lst_grads = lst_op.compute_gradients(loss=loss, var_list=last_layer_weights)
-    lst_train_op = lst_op.apply_gradients(grads_and_vars=lst_grads)
+    rsp_op = tf.train.AdamOptimizer(args.lr * lr_decay)
+    rsp_grads = all_op.compute_gradients(loss=rsp_loss, var_list=ip_weights)
+    rsp_train_op = all_op.apply_gradients(grads_and_vars=rsp_grads)
 
     weight_dict = {}
     for item in all_weights:
         if 'kernel' in item.name:
             weight_dict[item.name] = item
-    print('weights to be saved')
+        if 'bias' in item.name:
+            weight_dict[item.name] = item
     print(weight_dict)
 
     ph = {
@@ -165,37 +162,48 @@ def build_model(num_hidden, decay, activation):
         'y': y,
         'lr_decay': lr_decay,
         'is_training': is_training,
+        'layer2_copy': layer2_copy
     }
+    assign = {}
+    for i in range(len(num_hidden)):
+        kernel_var = weight_dict['network/nn/dense_{}/kernel:0'.format(i)]
+        bias_var = weight_dict['network/nn/dense_{}/bias:0'.format(i)]
+        ph['dense_{}_kernel'.format(i)] = tf.placeholder(dtype=tf.float32, 
+            shape=kernel_var.get_shape())
+        ph['dense_{}_bias'.format(i)] = tf.placeholder(dtype=tf.float32, 
+            shape=bias_var.get_shape())
+        assign['dense_{}_kernel'.format(i)] = tf.assign(kernel_var, ph['dense_{}_kernel'.format(i)])
+        assign['dense_{}_bias'.format(i)] = tf.assign(bias_var, ph['dense_{}_bias'.format(i)])
 
     targets = {
         'layers': layers,
         'all':{
             'weights': all_weights,
             'train': all_train_op,
-            'entropy_loss': entropy_loss,
-            'acc_loss': acc_loss,
+            'rsp_train': rsp_train_op,
+            'rsp_loss': rsp_loss,
+            'rmse_loss': rmse_loss,
             'update': update_ops,
             'reg_loss': reg,
         },
         'lst':{
             'weights': all_weights,
             'train': lst_train_op,
-            'entropy_loss': entropy_loss,
-            'acc_loss': acc_loss,
-            'update': update_ops,
+            'rmse_loss': rmse_loss,
+            'update': update_ops,       
             'reg_loss': reg
         },
         'eval':{
             'weights': weight_dict,
-            'entropy_loss': entropy_loss,
-            'acc_loss': acc_loss,
+            'rmse_loss': rmse_loss,
             'out': out,
-        }
+            'ip_weights': ip_weights
+        },
+        'assign': assign
     }
     for i in range(len(num_hidden)):
         targets['all']['reg{}_loss'.format(i)] = regs[i]
-        #if i > 0:
-        #    targets['all']['reg{}_loss'.format(i)] /= decay[i]
+        targets['all']['reg{}_loss_rsp'.format(i)] = regs_rsp[i]
     return ph, targets
 
 
@@ -211,27 +219,14 @@ else:
 RANGE = 2 * np.pi
 reg_func = cosc
 
-mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-train_images = mnist.train.images # Returns np.array
-train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-test_images = mnist.test.images # Returns np.array
-test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-
-train_images = train_images.reshape(-1, args.x_dim)
-train_labels = train_labels.reshape(-1)
-test_images = test_images.reshape(-1, args.x_dim)
-test_labels = test_labels.reshape(-1)
+ndata_train = 100000
+train_x, train_y = cosc_data(-RANGE, RANGE, 1.0, ndata_train)
+nlayers = len(num_hidden)
 
 # scaling
-mean_x = np.mean(train_images, 0, keepdims=True)
-std_x = np.std(train_images, 0, keepdims=True)
-train_images = (train_images - mean_x) / std_x
-test_images = (test_images - mean_x) / std_x
-
-ndata_train = train_images.shape[0]
-ndata_test = test_images.shape[0]
-
-nlayers = len(num_hidden)
+mean_x = np.mean(train_x, 0, keepdims=True)
+std_x = np.std(train_x, 0, keepdims=True)
+train_x = (train_x - mean_x) / std_x
 
 sess.run(tf.global_variables_initializer())
 
@@ -239,78 +234,68 @@ sess.run(tf.global_variables_initializer())
 def get_norm(u, axis=1):
     return np.sqrt(np.sum(np.square(u), axis))
 
-vis_x = test_images[:400, :]
-vis_y = test_labels[:400]
-tsne = manifold.TSNE(n_components=2, init='pca', random_state=501)
-vis_x_tsne = tsne.fit_transform(vis_x)
-vis_x_min, vis_x_max = vis_x_tsne.min(0), vis_x_tsne.max(0)
-vis_x_norm = (vis_x_tsne - vis_x_min) / (vis_x_max - vis_x_min)
 
 
-color_set = ['red', 'blue', 'yellow', 'green', 'gray', 'black', 'orange', 'purple', 'pink', 'skyblue']
-plt.figure(figsize=(8,8))
-for i in range(400):
-    plt.text(vis_x_norm[i, 0], vis_x_norm[i, 1], str(vis_y[i]), color=color_set[vis_y[i]], 
-        fontdict={'weight': 'bold', 'size': 7})
-plt.savefig(os.path.join(args.save_log_dir, 'embed.png'))
-plt.close()
-plt.clf()
-#[0, 1]
+def resample(prelayer, nextlayer, sample_weight):
+    # prelayer [n_{i-1}, n_i]
+    # nextlayer  [n_i, n_{i+1}]
+    ni = nextlayer.shape[0]
+    print('PRELAYER SHAPE = {}'.format(prelayer.shape))
+    print('NEXTLAYER SHAPE = {}'.format(nextlayer.shape))
+    assert ni == prelayer.shape[1]
+    assert ni == sample_weight.shape[0]
+    sel = np.random.choice(ni, ni, p=sample_weight)
 
-def kernel_regression(dx, dy, dv):
-    # auxiliary functions
-    def grid(self, x_l, x_r, x_steps, y_l, y_r, y_steps):
-        delta_x = (x_r - x_l) / (x_steps - 1)
-        delta_y = (y_r - y_l) / (y_steps - 1)
-        data = np.zeros((x_steps * y_steps, 2))
-        n = 0
-        for i in range(x_steps):
-            for j in range(y_steps):
-                data[n, :] = np.array([delta_x * i + x_l, delta_y * j + y_l])
-                n += 1
-        return data
+    prelayer_rsp = prelayer[:, sel]
+    nextlayer_rsp = nextlayer[sel, :] / (np.expand_dims(sample_weight[sel], 1) * ni)
+    return prelayer_rsp, nextlayer_rsp
 
-    def togrid(self, x, nx, ny):
-        data = np.zeros((nx, ny))
-        n = 0
-        for i in range(nx):
-            for j in range(ny):
-                data[j, i] = x[n]
-                n += 1
-        return data
 
-    xl = -2
-    xr = 2
-    data = grid(xl, xr, 20, xl, xr, 20)
-    sigma = (xr - xl) / 20.0
+def add_noise(theta, decay=1.0):
+    # theta [n_{i-1}, n_i]
+    n1, n2 = theta.shape[0], theta.shape[1]
+    return theta + np.random.normal(0, 0.1*decay/np.sqrt(theta.shape[0]), (n1, n2))
 
-    x = np.ogrid[-xl:xr:20j]
-    y = np.ogrid[-xl:xr:20j]
-    vset = {}
-    v = np.ones((20, 20)) * -1
-    for i in range(len(dx)):
-        cx = int((dx[i] + 2) / sigma)
-        cy = int((dy[i] + 2) / sigma)
-        if (cx, cy) in vset:
-            vset[(cx, cy)].append(dv[i])
-        else:
-            vset[(cx, cy)] = [dv[i]]
-    for i in range(20):
-        for j in range(20):
-            if (i, j) in vset:
-                v[i, j] = np.mean(vset[(i, j)])
 
-    return x, y, v
+def resample_nn(sess, ph, targets, nlayers):
+    weights = sess.run(targets['eval']['weights'])
+    ip = sess.run(targets['eval']['ip_weights'])
+    kernels = []
+    biases = []
+    for i in range(nlayers):
+        kernels.append(weights['network/nn/dense_{}/kernel:0'.format(i)])
+        biases.append(weights['network/nn/dense_{}/bias:0'.format(i)])
+    for i in range(nlayers):
+        curlayer = np.concatenate([kernels[i], biases[i]], 0)
+        curlayer_n, kernels[i + 1] = resample(curlayer, kernels[i + 1], ip[i])
+        kernels[i] = curlayer_n[:-1, :]
+        bias[i] = curlayer_n[-1, :]
+    feed = {}
+    for i in range(nlayers):
+        feed[ph['dense_{}_kernel'.format(i)]] = kernels[i]
+        feed[ph['dense_{}_bias'.format(i)]] = biases[i]
+    sess.run(targets['assign'], feed)
 
 
 if True:
-    accs = []
+    rmse = []
+
+    pp1 = []
+    for t in tqdm(range(ndata_train // args.batch_size)):
+        batch_x = train_x[t * args.batch_size: (t + 1) * args.batch_size, :]
+        batch_y = train_y[t * args.batch_size: (t + 1) * args.batch_size]
+
+        fetch = sess.run(targets['layers'], feed_dict={ph['is_training']: False, 
+            ph['x']: batch_x, ph['y']: batch_y}) #, ph['lr_decay']: args.decay**(epoch)})
+        pp1.append(fetch[2])
+    train_l2v = np.concatenate(pp1, 0)
+
 
     for epoch in range(args.num_epoches):
         test_info = {}
-        for t in tqdm(range(ndata_test // args.batch_size)):
-            batch_x = test_x[t * args.batch_size: (t + 1) * args.batch_size, :]
-            batch_y = test_y[t * args.batch_size: (t + 1) * args.batch_size]
+        for t in tqdm(range(ndata_train // args.batch_size)):
+            batch_x = train_x[t * args.batch_size: (t + 1) * args.batch_size, :]
+            batch_y = train_y[t * args.batch_size: (t + 1) * args.batch_size]
 
             fetch = sess.run(targets['eval'], feed_dict={ph['is_training']: False, 
                 ph['x']: batch_x, ph['y']: batch_y})
@@ -318,30 +303,45 @@ if True:
             fetch = sess.run(targets['layers'], feed_dict={ph['is_training']: False, 
                 ph['x']: batch_x, ph['y']: batch_y, ph['lr_decay']: args.decay**(epoch)})
 
-        accs.append(np.mean(test_info['acc_loss']))
+        rmse.append(np.mean(test_info['rmse_loss']))
         print_log('Test', epoch, test_info)
         
-        layers_value = sess.run(targets['layers'], feed_dict={ph['is_training']: True, ph['x']: vis_x})
+        xp = np.arange(400) / 400.0 * RANGE * 2 - RANGE
+        xp = np.expand_dims(xp, 1)
+        xx = (xp - mean_x) / std_x
+        yy = reg_func(xp)
+        out = sess.run(targets['eval']['out'], feed_dict={ph['is_training']: False, 
+            ph['x']: xx})
+
+        layers_value = sess.run(targets['layers'], feed_dict={ph['is_training']: True, ph['x']: xx})
         for i in range(nlayers):
             layer_i = layers_value[i]     #  [B, m]
             layer_norm = np.sqrt(np.sum(np.square(layer_i), 1))
             print('Dist of layer {} norm: mean = {}, std = {}'.format(i, np.mean(layer_norm), np.std(layer_norm)))
         
+        plt.figure(figsize=(6,6))
+        plt.plot(xp, yy, color="red")
+        plt.plot(xp, out, color="blue")
+        plt.xlim(-RANGE, RANGE)
+        #plt.ylim(-1.2,1.2)
+        plt.savefig(os.path.join(args.save_log_dir, 'pred_{}.png'.format(epoch)))
+        plt.close()
+        plt.clf()
+
         # distribution of u
-        u = sess.run(targets['eval']['weights']['network/dense_{}/kernel:0'.format(nlayers - 1)])    # [1000, 1]
+        u = sess.run(targets['eval']['weights']['network/nn/dense_{}/kernel:0'.format(nlayers - 1)])    # [1000, 1]
         u_norm = get_norm(u, 1)
         print('Distribution of u norm: {} +/- {}'.format(np.mean(u_norm), np.std(u_norm)))
         print(len(layers_value))        
         for layer_id in range(nlayers - 1): 
-            thetai = sess.run(targets['eval']['weights']['network/dense_{}/kernel:0'.format(layer_id)])
-            thetan = sess.run(targets['eval']['weights']['network/dense_{}/kernel:0'.format(layer_id + 1)])
+            thetai = sess.run(targets['eval']['weights']['network/nn/dense_{}/kernel:0'.format(layer_id)])
+            thetan = sess.run(targets['eval']['weights']['network/nn/dense_{}/kernel:0'.format(layer_id + 1)])
             thetan_norm = get_norm(thetan, 1)
             plt.figure(figsize=(8,8))
             lvi = layers_value[layer_id + 1]
             for i in range(100):
                 ax=plt.subplot(10,10,i+1)
-                px, py, pv = kernel_regression(vis_x_norm[:, 0], vis_x_norm[:, 1], lvi[:, i])
-                ax.contourf(px, py, pv, 20)
+                ax.scatter(np.squeeze(xp), lvi[:, i], color='r', s=0.2+0.7*thetan_norm[i]/np.max(thetan_norm))
                 ax.set_title('Sample %d: %.2f' % (i, u_norm[i]), fontsize=5)
                 ax.axis('off')
             plt.savefig(os.path.join(args.save_log_dir, 'f_{}_samples_{}.png'.format(layer_id, epoch)))
@@ -360,6 +360,9 @@ if True:
             thetanorm = get_norm(thetai, 0)
             print('Distribution of theta {} norm: {} +/- {}'.format(layer_id, np.mean(thetanorm), np.std(thetanorm)))
 
+        if epoch % 10 == 0:
+            resample_nn(sess, ph, targets, nlayers)
+        
         cur_idx = np.random.permutation(ndata_train)
         train_info = {}
         for t in tqdm(range(ndata_train // args.batch_size)):
